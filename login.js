6fb483b00707148bb43acc705391b7f1d305dc16
arno5 <arnoaghababyan@gmail.com>
2018-09-12 22:07:45 -0700

    Initial commit

---
+++ login.js
@@ -0,0 +1,11 @@
+// database stuff (incomplete. doesn't work)
+
+var email = "";
+var password = "";
+
+var http = require('http');
+http.createServer().listen(8000);
+
+// print entered info into console
+console.log("Email: ", email,
+    "\nPassword: ", password);
