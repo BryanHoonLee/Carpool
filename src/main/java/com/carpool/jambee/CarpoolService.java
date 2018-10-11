3a61dd703b202c7eab9580c99adb5f3d496712d9
Jaedim <Jaedim@users.noreply.github.com>
2018-10-11 15:56:37 -0700

    Set project directories up and added basic dependencies in gradle

---
+++ src/main/java/com/carpool/jambee/CarpoolService.java
@@ -0,0 +1,12 @@
+package com.carpool.jambee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolService {
+
+    public static void main (String[] args) {
+        SpringApplication.run(CarpoolService.class, args);
+    }
+}
