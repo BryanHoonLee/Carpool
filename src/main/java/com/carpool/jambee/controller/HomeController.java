8e763f0327c0a99dfc0b700a0fe93579ac3551d5
Jaedim <Jaedim@users.noreply.github.com>
2018-10-16 11:17:41 -0700

    Refactored code a bit

--- src/main/java/com/carpool/jambee/controller/HomeController.java
+++ src/main/java/com/carpool/jambee/controller/HomeController.java
@@ -1,16 +1,27 @@
 package com.carpool.jambee.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.validation.Valid;
 
 @Controller
 public class HomeController {
 
     @GetMapping({"/", "/index", "/home"})
     public String index() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        System.out.println(username);
+
         return "index";
     }
 
