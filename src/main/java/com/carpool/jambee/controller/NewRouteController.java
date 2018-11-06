183550fa83ac760f6518100b95747f05aad06582
Jaedim <Jaedim@users.noreply.github.com>
2018-11-06 14:10:40 -0800

    Tied add.html to backend to provide data to server
    Provides data to fit the POJO abstraction to hold the added user data, but there are still some things that are missing.
    POJO requires translation of certain data to be able to be saved, so that needs to be fixed.
    Example is daysOfWeek, where it requires inserting a boolean array rather than being able to accept raw data from the frontend.

---
+++ src/main/java/com/carpool/jambee/controller/NewRouteController.java
@@ -0,0 +1,44 @@
+package com.carpool.jambee.controller;
+
+import com.carpool.jambee.mongodb.model.Address;
+import com.carpool.jambee.mongodb.model.UserData;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class NewRouteController {
+    @PostMapping(value="/add/submit",
+                 consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String addNewRoute(
+            Model model,
+            UserData userData,
+            String startingCity,
+            String startingState,
+            String destinationCity,
+            String destinationState,
+            String day1, String day2, String day3,
+            String day4, String day5, String day6, String day7)
+    {
+        boolean daysOfWeek[] = {
+                day1 != null,
+                day2 != null,
+                day3 != null,
+                day4 != null,
+                day5 != null,
+                day6 != null,
+                day7 != null
+        };
+        Address startingAddress = new Address(startingCity, startingState);
+        Address destinationAddress = new Address(destinationCity, destinationState);
+
+        userData.setDaysOfWeek(daysOfWeek);
+        userData.setStartingAddress(startingAddress);
+        userData.setDestinationAddress(destinationAddress);
+
+        return "redirect:/add";
+    }
+
+
+}
