9b8079277433f9d49a79301d19f32dfeaae7ed51
Jaedim <Jaedim@users.noreply.github.com>
2018-11-25 00:05:42 -0800

    Added user settings page

--- src/main/java/com/carpool/jambee/controller/UserController.java
+++ src/main/java/com/carpool/jambee/controller/UserController.java
@@ -3,12 +3,14 @@ package com.carpool.jambee.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
     @GetMapping("/profile")
-    public String userProfile(Model model) {
-        return "profile";
+    public ModelAndView userProfile(ModelAndView modelAndView) {
+        modelAndView.setViewName("user_settings");
+        return modelAndView;
     }
 }
